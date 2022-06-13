6af20ca60ad23b828f2d832d30a8e5aa24a69cc6
jorgefperes <jorge-peres@live.com>
2022-06-12 22:09:34 -0300

    feat: with react query simple

---
+++ with-react-query-simple/pages/_app.js
@@ -0,0 +1,14 @@
+import '../styles/globals.css';
+import { useState } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+
+function MyApp({ Component, pageProps }) {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />;
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
